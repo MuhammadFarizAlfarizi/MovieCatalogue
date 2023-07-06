ab1288a2df87635d5f39905562fa1ec2520a9a8e
MuhammadFarizAlfarizi <ongafarizalfarizi123@gmail.com>
2023-07-07 00:31:26 +0700

    Initial commit

---
+++ app/src/main/java/com/fariz/moviecatalogue/DetailTvShowActivity.kt
@@ -0,0 +1,27 @@
+package com.fariz.moviecatalogue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_detail_movie.*
+
+class DetailTvShowActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_DATA = "extra_data"
+    }
+
+    var tvshow : TvShow? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_tv_show)
+
+        tvshow = intent.getParcelableExtra(EXTRA_DATA)
+
+        tv_title.text = tvshow?.name
+        tv_overview.text = tvshow?.overview
+        tv_rating.text = tvshow?.popularity
+        Glide.with(img_poster).load(IMAGE_BASE +
+                tvshow!!.poster).into(img_poster)
+    }
+}
---
+++ app/src/main/java/com/fariz/moviecatalogue/LinkModel.kt
@@ -0,0 +1,7 @@
+package com.fariz.moviecatalogue
+
+data class LinkModel(
+    val title: String,
+    val image: Int,
+    val url: String,
+)
